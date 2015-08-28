aef1edb85327cc99b93baf4dc9b54df6c91672c1
Patrick Samson <patricksamson236@gmail.com>
2015-08-28 19:55:12 -0400

    Added Datetime support to jQuery Bootgrid

--- views/datagrid.blade.php
+++ views/datagrid.blade.php
@@ -54,17 +54,32 @@
 		        date: {
 		            from: function (value) { return Date.parse(value); },
 		            to: function (value) { return new Date(value).toISOString().split('T')[0]; }
-		        }
+		        },
+                datetime: {
+                    from: function (value) { return Date.parse(value); },
+                    to: function (value) {
+                        function pad(number) {
+                          if (number < 10) {
+                            return '0' + number;
+                          }
+                          return number;
+                        }
+
+                        var d = new Date(value);
+                        return d.getUTCFullYear() +
+                            '-' + pad(d.getUTCMonth() + 1) +
+                            '-' + pad(d.getUTCDate()) +
+                            ' ' + pad(d.getUTCHours()) +
+                            ':' + pad(d.getUTCMinutes()) +
+                            ':' + pad(d.getUTCSeconds());
+                    }
+                },
 		    },
 			formatters: {
 				"link": function(column, row)
 				{
 					return '<a href="/admin/promotions/show/' + row.id + '">Voir</a>';
 				},
-				"date": function(column, row)
-				{
-					return row.dateDebut.split(' ')[0];
-				}
 			}
 		});
 	</script>
