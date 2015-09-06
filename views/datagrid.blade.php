b5ae865dd230ad1bb39504d9f26c02765d2cd4c0
Patrick Samson <patricksamson236@gmail.com>
2015-09-06 16:04:02 -0400

    Added Commands column formatter

--- views/datagrid.blade.php
+++ views/datagrid.blade.php
@@ -85,10 +85,14 @@
                 },
 		    },
 			formatters: {
-				"link": function(column, row)
-				{
-					return '<a href="/admin/promotions/show/' + row.id + '">Voir</a>';
-				},
+                "commands": function(column, row)
+                {
+                    return '<div class="btn-toolbar">\
+                                <a href="/view/' + row.id + '" class="btn btn-xs btn-default"><span class="fa fa-eye"></span></a>\
+                                <a href="/edit/' + row.id + '" class="btn btn-xs btn-warning"><span class="fa fa-pencil"></span></a>\
+                                <a href="/delete/' + row.id + '" class="btn btn-xs btn-danger"><span class="fa fa-trash-o"></span></a>\
+                            </div>';
+                },
 			}
 		});
 
