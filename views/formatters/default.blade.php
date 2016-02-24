b4db52cf68ceb03ee38300079727e06ba495232d
Patrick Samson <patricksamson236@gmail.com>
2016-02-24 12:26:34 -0500

    Improved actions formatter

--- views/formatters/default.blade.php
+++ views/formatters/default.blade.php
@@ -1,12 +1,18 @@
 <script type="text/javascript">
 function DatagridBuilder() {};
 
-DatagridBuilder.commands = function(value, row, index) {
+DatagridBuilder.actions = function(value, row, index) {
     return '<div class="btn-toolbar">\
-                <a href="/view/' + row.id + '" class="btn btn-xs btn-default"><span class="fa fa-eye"></span></a>\
-                <a href="/edit/' + row.id + '" class="btn btn-xs btn-warning"><span class="fa fa-pencil"></span></a>\
-                <a href="/delete/' + row.id + '" class="btn btn-xs btn-danger"><span class="fa fa-trash-o"></span></a>\
-            </div>';
+      @if (isset($data['formatters.actions.view']))
+        <a href="{!! $data['formatters.actions.view'] !!}" class="btn btn-xs btn-default"><span class="fa fa-eye"></span></a>\
+      @endif
+      @if (isset($data['formatters.actions.edit']))
+        <a href="{!! $data['formatters.actions.edit'] !!}" class="btn btn-xs btn-warning"><span class="fa fa-pencil"></span></a>\
+      @endif
+      @if (isset($data['formatters.actions.delete']))
+        <a href="{!! $data['formatters.actions.delete'] !!}" class="btn btn-xs btn-danger"><span class="fa fa-trash-o"></span></a>\
+      @endif
+    </div>';
 }
 
 DatagridBuilder.replaceSpecialChars = function(value) {
